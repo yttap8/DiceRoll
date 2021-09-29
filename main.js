7820b1ebf7b1f0de1d3231d5790c96e978860fda
yttap8 <moc.liamg.patrick@gmail.com>
2021-09-29 11:16:02 -0400

    edits

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
+//Creates aray of images
+var randomImage = new Array();
+randomImage [0] = "img/Dice1.png";
+randomImage [1] = "img/Dice2.png";
+randomImage [2] = "img/Dice3.png";
+randomImage [3] = "img/Dice4.png";
+randomImage [4] = "img/Dice5.png";
+randomImage [5] = "img/Dice6.png";
+
+//Picks random number 0-5
+function randomNumber(){
+  return Math.floor(Math.random()*6);
+}
+
+//Displays array image corresponding to random number
 function getRandomImage(){
-  var randomImage = new Array();
-  randomImage [0] = "img/Dice1.png";
-  randomImage [1] = "img/Dice2.png";
-  randomImage [2] = "img/Dice3.png";
-  randomImage [3] = "img/Dice4.png";
-  randomImage [4] = "img/Dice5.png";
-  randomImage [5] = "img/Dice6.png";
-  var number = Math.floor(Math.random()*6);
-  return document.getElementById("roll").src = randomImage[number]
+  document.getElementById("roll").src = randomImage[randomNumber()]
+}
+
+function displaySeveral (num){
+  for (var i = 0, i<num, i++){
+
+  }
 }
